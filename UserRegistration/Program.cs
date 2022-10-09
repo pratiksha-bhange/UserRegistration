5accd41389e46bba1866660f6b4e29155aab2425
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-10-09 15:44:36 +0530

    [Pratu]ADDED: Validating First Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,6 +7,12 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Programs using Regex");
+
+            Console.WriteLine("\nEnter the first name = ");
+            Console.WriteLine("\nNote: First name starts with Cap and has minimun 3 characters");
+            string firstName = Console.ReadLine();
+            UserGegistration userRegistration = new UserGegistration();
+            Console.WriteLine(userRegistration.validateFirstName(firstName));
         }
     }
 }
---
+++ UserRegistration/UserGegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserGegistration
+    {
+        public static string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+        public bool validateFirstName(String firstName)
+        {
+            return Regex.IsMatch(firstName, NAME_PATTERN);
+        }
+    }
+}
